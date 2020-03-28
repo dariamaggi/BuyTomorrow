11a183b753c97efb0c6596a974e3c8627db9d62c
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-03-28 19:30:48 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>My App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
