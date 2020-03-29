36b68ae072a59d3635545a20bcccd911835ba8cb
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-03-29 17:29:08 +0200

    barra

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,6 @@ const Home: React.FC = () => {
         <IonCol>
       <div id="begin" ><IonLabel>Inizia la ricerca!</IonLabel></div>
       <div id="parent">
-        
       <div id="child"> <IonSearchbar></IonSearchbar></div>
      
         
