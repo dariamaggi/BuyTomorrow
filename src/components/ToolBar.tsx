e5d98f3ef8d497d70835f7b566dd287147cf79f8
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-03-29 12:12:26 +0200

    added toolbar in row

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {IonButton, IonItem, IonTitle, IonToolbar, IonIcon} from '@ionic/react';
+import {IonButton, IonRow, IonItem, IonTitle, IonToolbar, IonIcon} from '@ionic/react';
 
 
 interface ContainerProps { }
@@ -8,14 +8,16 @@ const ToolBar: React.FC<ContainerProps> = () => {
     return (
         
         <IonToolbar>
-            <IonTitle>Buy2morrow</IonTitle>
-            <IonItem>
+             <IonRow>
+            <IonTitle size="large">Buy2morrow</IonTitle>
                 <IonButton>
                     <IonIcon name="home"></IonIcon>
                 </IonButton>
+                <IonButton fill="outline" type="submit"  routerLink="/login">Accedi
+                </IonButton>
                 <IonButton fill="outline" type="submit" routerLink="/register">Registrati</IonButton>
-                <IonButton fill="outline" type="submit" routerLink="/login">Accedi</IonButton>
-            </IonItem>
+            
+                </IonRow>
         </IonToolbar>
     );
 };
