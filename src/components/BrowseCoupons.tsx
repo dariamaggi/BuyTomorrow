d14b660f35f01fc821b0d808659394b8ff43985f
Riccardo Amadio <riccardo.amadio1@gmail.com>
2020-03-29 18:54:42 +0200

    BrowseCoupons

--- src/components/BrowseCoupons.tsx
+++ src/components/BrowseCoupons.tsx
@@ -1,13 +1,10 @@
 import React, {useState, useEffect, constructor} from 'react';
-import { IonCol, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCardContent, IonCard, IonCardHeader, IonSearchbar, IonCardSubtitle, IonCardTitle, IonThumbnail, IonGrid, IonRow } from '@ionic/react';
-
-
+import {IonCol, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCardContent, IonCard, IonCardHeader, IonSearchbar, IonCardSubtitle, IonCardTitle, IonThumbnail, IonGrid, IonRow, IonItem } from '@ionic/react';
 interface ContainerProps { }
-
 const BrowseCoupons: React.FC<ContainerProps> = () => {
-
+    const [it, setIt] = useState<JSX.Element[]>();
     const [value, setValue] = useState([]);
-
+    const [searchText, setSearchText] = useState('');
     fetch('https://raw.githubusercontent.com/rokity/BuyTomorrow/master/risorse/product.json')
         .then((response) => {
             return response.json();
@@ -15,32 +12,37 @@ const BrowseCoupons: React.FC<ContainerProps> = () => {
         .then((data) => {
             setValue(data)
         });
-
-
-    let items = []
-    for (const v of value) {
-        items.push(<IonCol col-1 col-xl-2 col-lg-3 col-md-4>
-            <IonCard routerLink={"./product?id="+v["id"]}>
-            <img src={v["img_url"]} />
-            <IonCardHeader>
-                <IonCardSubtitle>Coupon</IonCardSubtitle>
-                <IonCardTitle>{v["name"]}</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-                {v["description"]}
-            </IonCardContent>
-            </IonCard>
-        </IonCol>)
-        }
-
-return (
-    <IonGrid>
-        <IonRow>
-            {items}
-        </IonRow>
-    </IonGrid>
-    
-);
+    function load_cards(query: string) {  
+        let a:JSX.Element[] = []
+        value.forEach(function (item,index) {
+            let name:string = item["name"]
+            if(name.includes(query)){
+                let c = (<IonCol col-1 col-xl-2 col-lg-3 col-md-4>
+                    <IonCard routerLink={"./product?id="+item["id"]}>
+                    <img src={item["img_url"]} />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Coupon</IonCardSubtitle>
+                        <IonCardTitle>{item["name"]}</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        {item["description"]}
+                    </IonCardContent>
+                    </IonCard>
+                </IonCol>)
+                a.push(c)      
+            }
+        });
+        setIt(a)
+    }
+    return (
+        <div>
+            <IonSearchbar onIonChange={e => load_cards(e.detail.value!)}></IonSearchbar>
+            <IonGrid>
+                <IonRow>
+                    {it}
+                </IonRow>
+            </IonGrid>
+        </div>
+    );
 };
-
 export default BrowseCoupons;
