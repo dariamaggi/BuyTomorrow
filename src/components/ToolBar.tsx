e058b76ed0202db0231fcc8e9699ac31f4427826
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-03-29 15:02:13 +0200

    d

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -5,18 +5,22 @@ import {IonButton, IonRow, IonItem, IonTitle, IonToolbar, IonIcon} from '@ionic/
 interface ContainerProps { }
 
 const ToolBar: React.FC<ContainerProps> = () => {
+    let logout = () => {
+        localStorage.removeItem('user');
+        window.location.href ="./home"
+
+    }
+
+    
     return (
         
         <IonToolbar>
              <IonRow>
             <IonTitle size="large">Buy2morrow</IonTitle>
-                <IonButton>
-                    <IonIcon name="home"></IonIcon>
-                </IonButton>
-                <IonButton fill="outline" type="submit"  routerLink="/login">Accedi
-                </IonButton>
+                
+                <IonButton fill="outline" type="submit"  routerLink="/login">Login</IonButton>
                 <IonButton fill="outline" type="submit" routerLink="/register">Registrati</IonButton>
-            
+                <IonButton fill="outline" type="submit" onClick={() => logout()}>Logout </IonButton>
                 </IonRow>
         </IonToolbar>
     );
