5cb80bc5c52e60cdfb869e82a393560d07b1780e
Riccardo Amadio <riccardo.amadio1@gmail.com>
2020-03-29 17:52:52 +0200

    fix ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {IonThumbnail, IonCol,IonButton, IonSearchbar, IonContent, IonHeader, IonPage, IonLabel, IonButtons } from '@ionic/react';
+import { IonThumbnail, IonCol, IonButton, IonSearchbar, IonContent, IonHeader, IonPage, IonLabel, IonButtons } from '@ionic/react';
 import React from 'react';
 import ToolBar from '../components/ToolBar';
 import './Home.css';
@@ -11,19 +11,20 @@ const Home: React.FC = () => {
       <IonHeader>
         <ToolBar></ToolBar>
       </IonHeader>
-      
+
       <IonContent >
         <IonCol>
-      <div id="begin" ><IonLabel>Inizia la ricerca!</IonLabel></div>
-      <div id="parent">
-      <div id="child"> <IonSearchbar></IonSearchbar></div>
-     
-        
-      </div> 
-      </IonCol>
-   </IonContent>
-     
-    </IonPage>
+          <main>
+            <div>
+              <IonLabel>Inizia la ricerca!</IonLabel>
+              <p><IonSearchbar></IonSearchbar></p>
+              
+            </div>
+          </main>
+        </IonCol>
+      </IonContent>
+
+    </IonPage >
   );
 };
 
